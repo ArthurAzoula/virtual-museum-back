65bac7585426d744f9c8349c40cb98955c73d186
Bryan <bryanferrando59@gmail.com>
2023-12-27 14:23:28 +0100

    Added types

--- app/schemas/query.ts
+++ app/schemas/query.ts
@@ -1,9 +1,9 @@
 import { GraphQLObjectType } from 'graphql';
-import { UserResolver } from '../resolvers/user.resolver';
+import UserResolver from '../resolvers/user.resolver';
 
 export const Query = new GraphQLObjectType({
   name: 'Query',
   fields: {
-    user: UserResolver
+    ...UserResolver
   }
 });
