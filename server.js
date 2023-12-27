8f94a5ef2423546ee977e999e9564b4c86679c3e
Azoula Arthur <arthur.azoula@epitech.eu>
2023-12-27 12:00:23 +0100

    [+] Success connection without sequelize to psql database

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const { graphqlHTTP } = require('express-graphql');
 const query = require('./app/schemas/query');
 const { GraphQLSchema } = require('graphql');
 
+var pgp = require('pg-promise')();
+
+var db = pgp(process.env.DATABASE_URL);
+
+db.one('SELECT $1 AS value', 123)
+    .then(function (data) {
+        console.log('DATA:', data.value)
+    })
+    .catch(function (error) {
+        console.log('ERROR:', error)
+    });
+
+
 const app = express();
 app.use('/graphql', graphqlHTTP({
     schema: new GraphQLSchema({query}),
